4c5427eeafde8866b4cdfa468f0127c1023850b7
kelibst <kbooster17@gmail.com>
2023-01-11 07:30:36 +0000

    initialize tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -15,14 +15,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1 className="text-3xl font-bold underline" >Vite + React</h1>
+      <div className="underline">
+      some test
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
